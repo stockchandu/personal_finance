1430aaf6966f23f72b96cf7b26227dba8c86c373
Chandan Kumar Malik <malikchandan926@gmail.com>
2024-08-02 01:37:19 +0530

    Update TextField.js

--- src/component/common/TextField.js
+++ src/component/common/TextField.js
@@ -1,16 +1,17 @@
-import  React ,{useState} from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-export default function MPFTextField({ label, value ,formValue,setFormValue}) {
+
+export default function MPFTextField({ label, value, formValue, setFormValue }) {
   const [formVal, setFormVal] = useState(value);
   const handleChange = (event) => {
-    const {name,value} = event.target
+    const { name, value } = event.target;
     setFormVal(value);
-    setFormValue({[name]:value})
+    setFormValue({ [name]: value });
   };
 
   return (
-    <Grid item xs={12} md={4} lg={4}>
+    <Grid item xs={12} md={4}>
       <label>{label}</label>
       <TextField
         id="outlined-basic"
@@ -18,12 +19,12 @@ export default function MPFTextField({ label, value ,formValue,setFormValue}) {
         value={formVal}
         name={label}
         sx={{
-        '& .MuiOutlinedInput-input::placeholder': {
-          color: 'black',
-          opacity: 1, 
-        },
-      }}
-      onChange={handleChange}
+          '& .MuiOutlinedInput-input::placeholder': {
+            color: 'black',
+            opacity: 1,
+          },
+        }}
+        onChange={handleChange}
       />
     </Grid>
   );
